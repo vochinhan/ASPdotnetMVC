2882f974a7197d7dfc4559dc5eaede6997039096
vochinhan <112261999+vochinhan@users.noreply.github.com>
2023-11-23 19:42:36 +0700

    menu

---
+++ 63cntt4n2/Controllers/ModuleController.cs
@@ -0,0 +1,25 @@
+﻿using MyClass.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace _63cntt4n2.Controllers
+{
+    public class ModuleController : Controller
+    {
+        MenusDAO menusDAO = new MenusDAO();
+        // GET: Module
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult MainMenu()
+        {
+            return PartialView(menusDAO.getListByParentId(0));
+        }
+    }
+}
--- MyClass/Model/Menus.cs
+++ MyClass/Model/Menus.cs
@@ -9,34 +9,51 @@ using System.Threading.Tasks;
 namespace MyClass.Model
 {
     [Table("Menus")]
-
     public class Menus
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Tên menu không để trống")]
+        [Display(Name = "Tên Menu")]
         public string Name { get; set; }
 
+        [Display(Name = "Bảng dữ liệu")]
         public int? TableId { get; set; }
 
+        [Display(Name = "Kiểu Menu")]
         public string TypeMenu { get; set; }
 
+        [Display(Name = "Vị trí")]
         public string Position { get; set; }
 
+        [Display(Name = "Liên kết")]
         public string Link { get; set; }
 
+        [Display(Name = "Cấp cha")]
         public int? ParentId { get; set; }
 
+        [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
 
-        public int CreatedBy { get; set; }
-
+        [Display(Name = "Người tạo")]
+        [Required(ErrorMessage = "Người tạo không để trống")]
         public DateTime CreatedAt { get; set; }
 
-        public int UpdateBy { get; set; }
+        [Display(Name = "Người tạo")]
+        [Required(ErrorMessage = "Người tạo không để trống")]
+        public int CreatedBy { get; set; }
 
+        [Display(Name = "Ngày cập nhật")]
+        [Required(ErrorMessage = "Ngày cập nhật không để trống")]
         public DateTime UpdateAt { get; set; }
 
+        [Display(Name = "Người cập nhật")]
+        [Required(ErrorMessage = "Người cập nhật không để trống")]
+        public int UpdateBy { get; set; }
+
+        [Display(Name = "Trạng thái")]
+        [Required(ErrorMessage = "Trạng thái không để trống")]
         public int Status { get; set; }
 
     }
